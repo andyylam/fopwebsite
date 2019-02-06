ab91e430f62efec0663938c5e3d920768bd72f5f
andyylam <andyylam1997@gmail.com>
2019-02-06 22:39:49 +0800

    footer

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,8 +1,10 @@
 const menuBtn = document.querySelector(".hamburger");
 const mobilePopup = document.querySelector(".mobile-popup");
+const main = document.querySelector(".main");
 
 let showMenu = false;
 menuBtn.addEventListener("click", toggleMenu);
+main.addEventListener("click", toCloseMenu);
 
 function toggleMenu() {
     if (!showMenu) {
@@ -11,6 +13,15 @@ function toggleMenu() {
         menuBtn.classList.add("close");
         showMenu = true;
     } else {
+        mobilePopup.classList.remove("shown");
+        mobilePopup.classList.add("notshown");
+        menuBtn.classList.remove("close");
+        showMenu = false;
+    }
+}
+
+function toCloseMenu() {
+    if (showMenu) {
         mobilePopup.classList.remove("shown");
         mobilePopup.classList.add("notshown");
         menuBtn.classList.remove("close");
