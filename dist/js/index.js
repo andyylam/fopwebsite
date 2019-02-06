379596621efe4c2475f451d48c6159c06a34b326
andyylam <andyylam1997@gmail.com>
2019-02-06 14:59:07 +0800

    animations

--- dist/js/index.js
+++ dist/js/index.js
@@ -7,10 +7,12 @@ menuBtn.addEventListener("click", toggleMenu);
 function toggleMenu() {
     if (!showMenu) {
         mobilePopup.classList.add("shown");
+        mobilePopup.classList.remove("notshown");
         menuBtn.classList.add("close");
         showMenu = true;
     } else {
         mobilePopup.classList.remove("shown");
+        mobilePopup.classList.add("notshown");
         menuBtn.classList.remove("close");
         showMenu = false;
     }
